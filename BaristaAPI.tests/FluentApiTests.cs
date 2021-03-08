a5476b5e989f420b91bf001128f0708b1a6d5fdd
Magnusson <Magnusson-5@hotmail.com>
2021-03-08 13:02:26 +0100

    Added Xunit Test

---
+++ BaristaAPI.tests/FluentApiTests.cs
@@ -0,0 +1,21 @@
+﻿using BaristaApi.Beverages;
+using Xunit;
+
+namespace BaristaAPI.tests
+{
+    public class FluentApiTests
+    {
+        [Fact]
+        public void When_AddingWaterAndBeans_Expect_Espresso()
+        {
+            // Arrange
+            //string expected = "Espresso";
+
+            // Act
+            var beverage = new FluentEspresso().AddWater(20).AddBeans().ToBeverage();
+
+            // Assert
+            Assert.IsType<Espresso>(beverage);
+        }
+    }
+}
